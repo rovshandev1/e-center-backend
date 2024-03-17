d6dfbca0287c31488c84e55c2fea4d2e7579883c
Ravshanbek <oravshan26@gmail.com>
2024-03-18 00:55:29 +0500

    fix it

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,7 @@ const login = async (req, res) => {
 			process.env.JWT_SECRET,
 			{ expiresIn: '1d' }
 		)
-		res.status(200).json({ token })
+		res.status(200).json({ token, role: user.role })
 	} catch (err) {
 		res.status(500).json({ message: 'Something went wrong' })
 	}
