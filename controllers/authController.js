a4e575b7cd5df4fbc6b82921d395b49eacd90a50
Ravshanbek <oravshan26@gmail.com>
2024-04-04 14:00:20 +0500

    ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,7 @@ const login = async (req, res) => {
 		)
 		res.status(200).json({ token, role: user.role, userId: user._id })
 	} catch (err) {
-		res.status(500).json({ message: 'Something went wrong' })
+		res.status(500).json({ message: 'Something went wrong', err })
 	}
 }
 
