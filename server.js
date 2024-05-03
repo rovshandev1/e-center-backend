e15110d5575a1efcb2f5754abfc098e657b0d7ee
Ravshanbek <oravshan26@gmail.com>
2024-05-03 10:33:36 +0500

    reverse

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const serverless = require("serverless-http")
 const app = express()
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
@@ -47,5 +46,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
-
-module.exports.handler = serverless(app);
