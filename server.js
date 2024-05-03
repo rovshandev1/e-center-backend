bd35b392063ab7ceef9891add5f674113fee8d04
Ravshanbek <oravshan26@gmail.com>
2024-05-03 10:26:33 +0500

    added new ver

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const serverless = require("serverless-http")
 const app = express()
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
@@ -46,3 +47,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
+
+module.exports.handler = serverless(app);
